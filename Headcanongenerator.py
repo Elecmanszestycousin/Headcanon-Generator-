bec0e7e17d398485cc44e6bb0336a6ab4f819b8a
TheIceBaby!!! <julianne.wien@gmail.com>
2025-11-22 16:18:58 -0600

    Add files via upload

---
+++ Headcanongenerator.py
@@ -0,0 +1,97 @@
+#Headcanon generator by TheIceBaby!!!! enjoy ;)
+import random
+print("Type in the character's name below.")
+CharacterName = input()
+print("")
+while True:
+	random_number = random.randint(1,50)
+	if random_number == 1:
+		print(CharacterName,"likes eating wooden chairs.")
+		break
+	if random_number == 2:
+		print(CharacterName,"likes their socks wet.")
+		break
+	if random_number == 3:
+		print(CharacterName,"burnt down an orphanage and got away with it.")
+		break
+	if random_number == 4:
+		print(CharacterName,"is completely obsessed with MegaMan X")
+		break
+	if random_number == 5:
+		print(CharacterName,"went to Brazil with Sean Combs as their driver.... it did NOT turn out well...")
+		break
+	if random_number == 6:
+		print(CharacterName,"is secretly cheating on their diet.")
+		break
+	if random_number == 7:
+		print(CharacterName,"is secretly a wizard.")
+		break
+	if random_number == 8:
+		print(CharacterName,"screams 67 every 6-7 seconds.")
+		break
+	if random_number == 9:
+		print(CharacterName,"plays DnD in their free time.")
+		break
+	if random_number == 10:
+		print(CharacterName,"is an online Roblox predator.")
+		break
+	if random_number == 11:
+		print(CharacterName,"DREAMS of writing an erratic fanfic of Master Khoga and Sooga from the Yiga Clan.")
+		break
+	if random_number == 12:
+		print(CharacterName,"LOVES brainrot.")
+		break
+	if random_number == 13:
+		print(CharacterName,"went to a P. DIddy concert.")
+		break
+	if random_number == 14:
+		print(CharacterName,"is secretly obsessed with K-POP.")
+		break
+	if random_number == 15:
+		print("You're a wizard,",CharacterName)
+		break
+	if random_number == 16:
+		print(CharacterName,"went on a road trip to California with Drake.")
+		break
+	if random_number == 17:
+		print(CharacterName+"'s motto is \"Not speed, SPED.\"" )
+		break
+	if random_number == 18:
+		print(CharacterName+"'s long lost brother is actually David Basucki.")
+		break
+	if random_number == 19:
+		print(CharacterName,"owns a collection of bazookas.")
+		break
+	if random_number == 20:
+		print(CharacterName,"looks up to the Saja Boys as their idol\(they're not even f----ing real i swear\"")
+		break
+	if random_number == 21:
+		print(CharacterName,"got an Uber from Shadow the Hedgehog\(yikes.\)")
+		break
+	if random_number == 22:
+		print(CharacterName,"weighs approximately 300 pounds on a scale.")
+		break
+	if random_number == 23:
+		print(CharacterName,"has the bubonic plague.")
+		break
+	if random_number == 24:
+		print(CharacterName,"loves listening to the song \"Doot Doot\"")
+		break
+	if random_number == 25:
+		print(CharacterName,"is a sweat at SSBU\(Super Smash Bros Ultimate\)")
+		break
+	if random_number == 26:
+		print(CharacterName,"will embark on a heroic quest to save their TV remote!")
+		break
+	if random_number == 27:
+		print(CharacterName,"says it's TVTIME!!!!")
+		break
+	if random_number == 28:
+		print(CharacterName,"got a low taper fade on a Sunday Morning")
+		break
+	if random_number == 29:
+		print(CharacterName,"went to church only for the bread.")
+		break
+	if random_number == 30:
+		print(CharacterName,"says \"GOO GOO GA GA\"")
+		break
